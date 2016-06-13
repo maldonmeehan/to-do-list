c5c5b9845ee0a931f8b104bb218c80070f1f8d4b
Benjamin Logue and Maldon Meehan <benjamin+maldon@>
2016-06-13 15:41:07 -0700

    it will add function cross out item on click

--- js/scripts.js
+++ js/scripts.js
@@ -8,38 +8,39 @@ $(document).ready(function(){
 
   $("#new-list").submit(function(event){
     event.preventDefault();
+
     var userInputtedTask = $("#new-list-item").val();
 
     var newUserList = new ListItem(userInputtedTask);
 
-    $("ul#list-items").append("<div class='list-container sortable'><li><span class='to-do-item'>" + newUserList.item  + "</span><input type='text' class='edit-item'></input></li>"  + "<button class='remove-item'>Remove</button>" + "<button class='complete-item'>Done</button></div>" );
+    $("ul#list-items").prepend("<div class='list-container sortable'><li><span class='to-do-item'>" + newUserList.item + "</span><input type='text' class='edit-item'></input></li>" + "<button class='remove-item btn btn-danger'>X</button>" + " " + "<button class='complete-item btn btn-success'>✓</button></div>");
 
     // Remove task from to do list
+
     $(".remove-item").click(function(){
       $(this).parent().remove();
     });
 
     // Mark task as completed on to do list
     $(".complete-item").click(function(){
-      $(this).parent().toggleClass("completed");
+      //$(this).last().toggleClass("completed");
+      $(this).parent().children("li").addClass("completed");
     });
+
     // Edit task on to do list
     $(".to-do-item").click(function(){
-
       $(this).hide().siblings('.edit-item').show().val($(this).text()).focus();
 
       $(".edit-item").focusout(function(){
         $(this).hide().siblings(".to-do-item").show().text($(this).val());
       });
 
+
     });
     // Reorder tasks on to do list
-
        $( "#sortable-list" ).sortable({
           items: '.sortable'
        });
-
-
-
+       $("#new-list")[0].reset();
   });
 });
